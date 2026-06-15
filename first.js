1fe47ab165938b4b7caea6302c9ac01acea71157
laxmi-rathour <laxmi2008bo@gmail.com>
2026-06-15 13:14:24 +0530

    input

--- first.js
+++ first.js
@@ -22,3 +22,54 @@ let forsale = true;
 
 console.log(typeof online);
 console.log(`Is is car for sale: ${forsale}`);
+
+
+// arithmetic operators = operands ( values variables , etc.)
+//        operators(+, - , *, /)
+//        ex. 11 = x+;
+
+// let students = 31;
+
+// students = students + 1;
+// students = students - 1;
+// students = students / 2;
+// students = students * 3;
+// students = students ** 4;
+// students= students % 2;
+
+//  let extraStudents =  students % 3;
+// console.log(extraStudents);
+
+// students += 1;
+// students -=1;
+// students *=2;
+// students %=2;
+// students **=3;
+// students /= 2;
+// students --;
+// students ++;
+
+// console.log(students);
+
+/*
+   operator precedence
+   1. parenthesis()
+   2. exponents
+   3. multiplication & division & modulo
+   4. addition & subtration
+*/
+
+let result = 12 % 5 + 8/2;
+console.log( `Result of this is : ${result}`);
+
+let solution = 6 / 2 **(2 +5);
+console.log(`Solution is: ${solution}`);
+
+// How to accept user input 
+// 1. EASY WAY = WINDOW promot
+//  2. PROFESSTIONAL MAY = HTML Textbox 
+
+/*let username;   // this is the first way 
+username = window.prompt("what's your username?");
+console.log(username);*
+
