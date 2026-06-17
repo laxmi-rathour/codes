475a71377b262896975b297c23fc678fbfa94827
laxmi-rathour <laxmi2008bo@gmail.com>
2026-06-17 14:26:30 +0530

    your message

--- circumference.js
+++ circumference.js
@@ -4,6 +4,7 @@ let circumference;
 
 document.getElementById("mySubmit").onclick = function(){
      radius = document. getElementById("myText").value;
+    
      radius = Number(radius);
     circumference = 2 * PI * radius;
 
